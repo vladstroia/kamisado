4aa08a5b1b6eae8935f9ae2bc0a9148d54e2a2ab
vlastroia <vlad.stroia@gmail.com>
2019-07-29 23:58:45 +0300

    add ring to circles

--- draw_circle.js
+++ draw_circle.js
@@ -1,1 +1,3 @@
 table.rows[4].cells[1].firstElementChild.classList.add('circle');
+table.rows[4].cells[1].firstElementChild.style.background = 'white';
+table.rows[4].cells[1].firstElementChild.style.border = '1vh solid blue';
